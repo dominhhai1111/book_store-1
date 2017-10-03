5c072dd8bf0c2cf1303ecf15542cadc62001fc8c
= <=>
2017-10-03 16:08:38 +0700

    Add project

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <base href="{{asset('')}}" />
     <meta charset="utf-8">
-    <title>Lá đỏ Book</title>
+    <title>Book Store</title>
 
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
